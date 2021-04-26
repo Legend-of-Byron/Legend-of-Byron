2a788b118e2f13a22168d034a523927b2cd22a63
velberhand <velberhand@protonmail.com>
2021-04-26 23:13:04 +0200

    added imagelist csv in build

--- scripts/buildChaptersMetadata.js
+++ scripts/buildChaptersMetadata.js
@@ -3,6 +3,13 @@ const args = process.argv.slice(2);
 const NB_ARGS_PER_CHAPTER = 3;
 const chaptersMetadata = [];
 
+const imagelistMap = new Map(fs
+    .readFileSync(__dirname + '/../imagelist.csv', { encoding: 'utf8'})
+    .split('\n')
+    .map(row => row.split(',')));
+
+console.log(Array.from(imagelistMap.entries()));
+
 for(let i = 0; i < args.length; i += NB_ARGS_PER_CHAPTER) {
     const [filename, createdAt, updatedAt] = args.slice(i, i + NB_ARGS_PER_CHAPTER);
 
@@ -11,13 +18,15 @@ for(let i = 0; i < args.length; i += NB_ARGS_PER_CHAPTER) {
     }
 
     const [prefix, chapterNumber, title, status] = filename.split('__');
+    const order = Number(chapterNumber);
     chaptersMetadata.push({
-        order: Number(chapterNumber),
+        order,
         title: title.replace(/_/g, ' '),
         key: [prefix, chapterNumber, title].join('-'),
         status,
         createdAt: new Date(createdAt).toISOString(),
-        updatedAt: new Date(updatedAt).toISOString()
+        updatedAt: new Date(updatedAt).toISOString(),
+        url: (imagelistMap.get(chapterNumber) || '').replace(/"/g, '')
     });
 }
 
